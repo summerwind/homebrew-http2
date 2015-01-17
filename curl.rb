3e48dec7f5add7a8353633cc84a8a494b742c1ff
Moto Ishizawa <summerwind.jp@gmail.com>
2015-01-17 23:26:42 +0900

    Add curl formula

---
+++ curl.rb
@@ -0,0 +1,80 @@
+class Curl < Formula
+  homepage "http://curl.haxx.se/"
+  url "http://curl.haxx.se/download/curl-7.40.0.tar.bz2"
+  mirror "http://ftp.sunet.se/pub/www/utilities/curl/curl-7.40.0.tar.bz2"
+  sha256 "899109eb3900fa6b8a2f995df7f449964292776a04763e94fae640700f883fba"
+
+  bottle do
+    cellar :any
+    sha1 "a5ba00d51a113752e962ba8ee0911c1c496d6cbc" => :yosemite
+    sha1 "c36652236b7b624913370adf309c9e838c571018" => :mavericks
+    sha1 "624a82135884081cef194e5313e0652a16b240b0" => :mountain_lion
+  end
+
+  keg_only :provided_by_osx
+
+  option "with-libidn", "Build with support for Internationalized Domain Names"
+  option "with-rtmpdump", "Build with RTMP support"
+  option "with-libssh2", "Build with scp and sftp support"
+  option "with-c-ares", "Build with C-Ares async DNS support"
+  option "with-gssapi", "Build with GSSAPI/Kerberos authentication support."
+  option "with-libmetalink", "Build with libmetalink support."
+  option "with-libressl", "Build with LibreSSL instead of Secure Transport or OpenSSL"
+
+  deprecated_option "with-idn" => "with-libidn"
+  deprecated_option "with-rtmp" => "with-rtmpdump"
+  deprecated_option "with-ssh" => "with-libssh2"
+  deprecated_option "with-ares" => "with-c-ares"
+
+  depends_on "pkg-config" => :build
+  depends_on "libidn" => :optional
+  depends_on "rtmpdump" => :optional
+  depends_on "libssh2" => :optional
+  depends_on "c-ares" => :optional
+  depends_on "libmetalink" => :optional
+  depends_on "libressl" => :optional
+  depends_on "summerwind/http2/openssl"
+  depends_on "summerwind/http2/nghttp2"
+
+  def install
+    # Throw an error if someone actually tries to rock both SSL choices.
+    # Long-term, make this singular-ssl-option-only a requirement.
+    if build.with?("libressl") && build.with?("openssl")
+      ohai <<-EOS.undent
+      --with-openssl and --with-libressl are both specified and
+      curl can only use one at a time; proceeding with openssl.
+      EOS
+    end
+
+    args = %W[
+      --disable-debug
+      --disable-dependency-tracking
+      --disable-silent-rules
+      --prefix=#{prefix}
+    ]
+
+    args << (build.with?("libssh2") ? "--with-libssh2" : "--without-libssh2")
+    args << (build.with?("libidn") ? "--with-libidn" : "--without-libidn")
+    args << (build.with?("libmetalink") ? "--with-libmetalink" : "--without-libmetalink")
+    args << (build.with?("gssapi") ? "--with-gssapi" : "--without-gssapi")
+    args << (build.with?("rtmpdump") ? "--with-librtmp" : "--without-librtmp")
+    args << "--with-nghttp2"
+
+    if build.with? "c-ares"
+      args << "--enable-ares=#{Formula["c-ares"].opt_prefix}"
+    else
+      args << "--disable-ares"
+    end
+
+    system "./configure", *args
+    system "make", "install"
+  end
+
+  test do
+    # Fetch the curl tarball and see that the checksum matches.
+    # This requires a network connection, but so does Homebrew in general.
+    filename = (testpath/"test.tar.gz")
+    system "#{bin}/curl", stable.url, "-o", filename
+    filename.verify_checksum stable.checksum
+  end
+end
