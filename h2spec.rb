42214505b2af9f7f6e69930f8199fc1de985fb71
Moto Ishizawa <summerwind.jp@gmail.com>
2015-01-29 00:07:00 +0900

    Update h2spec to v0.0.4

--- h2spec.rb
+++ h2spec.rb
@@ -1,16 +1,16 @@
 require 'formula'
 
-HOMEBREW_H2SPEC_VERSION='0.0.3'
+HOMEBREW_H2SPEC_VERSION='0.0.4'
 
 class H2spec < Formula
   homepage 'https://github.com/summerwind/h2spec'
 
   if OS.mac?
     url "https://github.com/summerwind/h2spec/releases/download/v#{HOMEBREW_H2SPEC_VERSION}/h2spec_darwin_amd64.zip"
-    sha1 "e59922fb0f93b70a4e2ca3fbf713d8fc08960822"
+    sha1 "bbb81e9f6802a29c5bf8e1e57ba1c764d9a71eb0"
   elsif OS.linux?
     url "https://github.com/summerwind/h2spec/releases/download/v#{HOMEBREW_H2PSEC_VERSION}/h2spec_linux_amd64.zip"
-    sha1 "3c7e8363d5ad97d5e49c2a01197a8c3be4cb9aa6"
+    sha1 "6e915537093e3c13576a8f804492ecb3dcaf9a47"
   end
 
   version HOMEBREW_H2SPEC_VERSION
